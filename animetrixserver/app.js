8753048532ba2932ebfb1780ff4db9268cf63b58
devil07625 <antivirus076@gmail.com>
2023-01-12 14:43:48 +0530

    23 Custon Error Handler complete

--- animetrixserver/app.js
+++ animetrixserver/app.js
@@ -11,8 +11,12 @@ const app = express();
 // Importing and Using Routes
 import anime from "./routes/AnimeRoutes.js";
 import user from "./routes/userRoutes.js";
+import ErrorMiddleware from "./middlewares/Error.js";
 
 app.use("/api/v1", anime);
 app.use("/api/v1", user);
 
 export default app;
+
+
+app.use(ErrorMiddleware);
--- animetrixserver/controllers/animeController.js
+++ animetrixserver/controllers/animeController.js
@@ -1,11 +1,12 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import {Anime} from "../models/Anime.js";
 
 
 
-export const getAllAnimes = async (req,res,next) =>{  
+export const getAllAnimes = catchAsyncError(async (req,res,next) =>{  
     const animes =await Anime.find();
     res.status(200).json({
         sucess: true,
         animes,
     });
-};
+});
