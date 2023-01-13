a431bbcba6af4fcdc8f33d62ded731636a5c43b7
devil07625 <antivirus076@gmail.com>
2023-01-13 15:14:41 +0530

    25 Registration Api complete

--- animetrixserver/models/User.js
+++ animetrixserver/models/User.js
@@ -1,59 +1,63 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
-
+import jwt from 'jsonwebtoken';
 const schema = new mongoose.Schema({
-
-    name:{
-        type:String,
-        required:[true,"Please enter your name"]
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  subscription: {
+    id: String,
+    status: String,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:[true,"Please enter your email"],
-        unique:true,
-        validate:validator.isEmail,
+    url: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:[true,"Please enter your password"],
-        minlength:[6,"Minimum password length is 6 characters"],
-        select:false,
-
+  },
+  playlist: [
+    {
+      anime: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Anime",
+      },
+      poster: String,
     },
-    role:{
-        type:String,
-        enum:["admin","user"],
-        default:"user",
-    },
-    subscription:{
-        id:String,
-        status:String,
-    },
-    avatar:{
-        public_id:{
-            type:String,
-            required:true,
-        },
-        url:{
-            type:String,
-            required:true,
-        }
-    },
-    playlist:[
-        {
-            anime:{
-                type:mongoose.Schema.Types.ObjectId,
-                ref:"Anime",
-            },
-            poster:String,
-        },
-    ],
-    createdAt:{
-        type:Date,
-        default:Date.now,
-    },
-    ResetPasswordToken:String,
-    ResetPasswordExpire:String,
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  ResetPasswordToken: String,
+  ResetPasswordExpire: String,
 });
+
+schema.methods.getJWTToken = function(){
+    return jwt.sign({_id:this._id}, process.env.JWT_SECRET, {
+        expiresIn:"15d",
+    });
+};
 
 export const User = mongoose.model("User", schema);
--- animetrixserver/routes/userRoutes.js
+++ animetrixserver/routes/userRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { register } from "../controllers/userController.js";
 
 
 const router = express.Router();
+
+// Register a user
+router.route("/register").post(register);
+
+
 
 
 export default router;
