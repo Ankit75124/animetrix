eeb2aada092bb9621e80827f2dcf43cd78898d64
devil07625 <antivirus076@gmail.com>
2023-01-15 18:26:39 +0530

    36 Payment Verification api routes complete

--- animetrixserver/controllers/paymentController.js
+++ animetrixserver/controllers/paymentController.js
@@ -2,6 +2,8 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/User.js";
 import { instance } from "../server.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import crypto from "crypto";
+import { Payment } from "../models/Payment.js";
 
 
 export const buysubscription = catchAsyncError(async (req, res, next) => {
@@ -25,5 +27,50 @@ export const buysubscription = catchAsyncError(async (req, res, next) => {
     res.status(201).json({
         success: true,
         subscriptionId:subscription.id,
+    });
+});
+
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+
+    const {
+      razorpay_signature,
+      razorpay_payment_id,
+      razorpay_subscription_id
+    } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+
+  const subscription_id = user.subscription.id;
+
+  const generated_signature = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET_KEY).update(razorpay_payment_id + "|" +subscription_id, "utf-8").digest("hex");
+
+  const isAuthentic = generated_signature === razorpay_signature;
+
+  if(!isAuthentic){
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
+  }
+
+//   adding into database
+  await Payment.create({
+    razorpay_signature,
+    razorpay_payment_id,
+    razorpay_subscription_id,
+  });
+
+  user.subscription.status = "active";
+    await user.save();
+
+  
+
+  
+    res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`);
+ 
+});
+
+export const getRazorpayKey = catchAsyncError(async (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        key:process.env.RAZORPAY_API_KEY,
     });
 });
--- animetrixserver/routes/paymentRoutes.js
+++ animetrixserver/routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { buysubscription } from "../controllers/paymentController.js";
+import { buysubscription, getRazorpayKey, paymentVerification } from "../controllers/paymentController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 
@@ -7,6 +7,12 @@ const router = express.Router();
 
 // Buy Subscription
 router.route("/subscribe").get(isAuthenticated, buysubscription);
+
+// Payment Verification and save reference in database
+router.route("/paymentverification").post(isAuthenticated, paymentVerification);
+
+// get razorpay key
+router.route("/razorpaykey").get(getRazorpayKey);
 
 
 export default router;
